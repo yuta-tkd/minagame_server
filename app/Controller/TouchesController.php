279ec7cd2476eacc80f5e2427d554e350ef79688
yuta <ley.ciel@gmail.com>
2015-08-08 21:10:35 +0900

    csrf

--- app/Controller/TouchesController.php
+++ app/Controller/TouchesController.php
@@ -7,14 +7,6 @@ class TouchesController extends AppController {
 
   public $uses = array('Touch','Edison');
 
-  public function beforeFilter(){
-    parent::beforeFilter();
-
-
-      $this->Security->csrfCheck = false;
-      $this->Security->validatePost = false;
-    
-	}
 
 
   // (GET) /touchs.format
