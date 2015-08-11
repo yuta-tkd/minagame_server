5c94ac593bbe9de07182112b6119ebb0d80f3c0e
yuta <ley.ciel@gmail.com>
2015-08-11 14:06:44 +0900

    duration

--- app/Controller/ApiController.php
+++ app/Controller/ApiController.php
@@ -80,7 +80,7 @@ class ApiController extends AppController {
 
     $this->viewClass = 'Json';
     $this->set(compact('allSensors'));
-    $this->set('_serialize', 'allSensors');
+    $this->set('_serialize', array('allSensors'));
   }
 
 
